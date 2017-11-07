78a2ca0f0360aca437f23e5df4b8d2d8722a0197
Danzel Rana <danzelrana@gmail.com>
2017-11-06 20:58:34 -0800

    test commit to branch: test1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/Header';
 
 // Create a Component
-const App = () => <Header headerText={'Albums123'} />;
+const App = () => <Header headerText={'Albums13'} />;
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
