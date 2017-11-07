7233e27c53c36f7ee18181132e7f065e897631a1
Danzel Rana <danzelrana@gmail.com>
2017-11-06 22:05:46 -0800

    test commit for pull

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Import a library to help create a Component
-import React from "react";
-import { AppRegistry } from "react-native";
-import Header from "./src/components/Header";
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
 // Create a Component
-const App = () => <Header headerText={"Albums"} />;
+const App = () => <Header headerText={'Albums!'} />;
 
 // Render it to the device
-AppRegistry.registerComponent("albums", () => App);
+AppRegistry.registerComponent('albums', () => App);
